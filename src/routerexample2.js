7edbec8364244961f01698a4a13891139c2fec3d
Shan, Yuhan <156050509+fqxcom@users.noreply.github.com>
2024-07-10 09:33:25 +0800

    Add files via upload

---
+++ src/routerexample2.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  useParams
+} from "react-router-dom";
+
+export default function App() {
+  return (
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+
+          {/* Nestes Routes */}
+          <Route path="/topics" element={<Topics />}>
+            <Route path=":topicId" element={<Topic />} />
+            <Route path="" element={<Info />} />
+          </Route>
+
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+      </div>
+    </Router>
+  );
+}
+
+
+
+// add topic function
+function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to="cats">Cats</Link>
+        </li>
+        <li>
+          <Link to="dogs">Dogs</Link>
+        </li>
+      </ul>
+
+      {/* Render child routes */}
+      <Outlet />
+
+    </div>
+  )
+  
+}
+function Home() {
+  return <h2>Home</h2>
+}
+function About() {
+  return <h2>About</h2>
+}
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
+function Info() {
+  return <h3>Please select a topic!</h3>
+}
